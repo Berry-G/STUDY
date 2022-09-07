1d8af2311c461bd6410b0828ffb1ac53055813ae
Young-Chae KIM <69233747+Berry-G@users.noreply.github.com>
2022-09-07 10:54:40 +0900

    chap18 commit

---
+++ Chap18(함수형 프로그래밍)/src/kr/co/ezenac/lambda/TestAdd.java
@@ -0,0 +1,15 @@
+package kr.co.ezenac.lambda;
+
+public class TestAdd
+{
+	public static void main(String[] args)
+	{
+		//Add addF = (int x, int y) -> {return x+y};
+		//Add addF = (int x, int y) -> x+y;
+		Add addF = (x,y) -> x+y;
+		
+		System.out.println(addF.add(3, 5));
+		
+	}
+
+}
