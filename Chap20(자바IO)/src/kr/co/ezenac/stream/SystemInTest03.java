b537915f882fea124c4789734a9e75c768161e57
Young-Chae KIM <69233747+Berry-G@users.noreply.github.com>
2022-09-22 17:27:56 +0900

    while 조건 수정

--- Chap20(자바IO)/src/kr/co/ezenac/stream/SystemInTest03.java
+++ Chap20(자바IO)/src/kr/co/ezenac/stream/SystemInTest03.java
@@ -3,15 +3,17 @@ package kr.co.ezenac.stream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class SystemInTest02
+public class SystemInTest03
 {
 	public static void main(String[] args)
 	{
 		System.out.println("알파벳 여러 개를 쓰고 [Enter]를 누르세요");
 		int i;
+		System.out.println("종료하려면 '끝' 이라고 쓰세요");
 		try
 		{
-			while((i = System.in.read()) != '\n')
+			InputStreamReader isr = new InputStreamReader(System.in);
+			while((i=isr.read()) != '끝')
 			{
 				System.out.print((char)i);
 			}
