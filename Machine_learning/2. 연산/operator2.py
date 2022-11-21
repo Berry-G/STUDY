9de6f14e05a99ab1a8f3e151d398b19c1023e984
Young-Chae KIM <69233747+Berry-G@users.noreply.github.com>
2022-11-21 12:17:17 +0900

    Add files via upload

---
+++ Machine_learning/2. 연산/operator2.py
@@ -0,0 +1,13 @@
+# 비교 연산
+print(2 > 3)    #false
+print(15 < 30)  #true
+print(1.5 >= 0) #true
+print(3 <= 3)   #true
+print("주변음" == "주변옴")   #false
+print("주변음" == "주변음")   #false
+print("1111111111" == "1111111111") #ture
+
+# 논리 연산
+print(4 < 6 and 10 >= 10)   #true and ture => true
+print("적용형 주변음 허용 모드" != "적용형 주변음 허용 모드" or "나는 할수 있다" == "나는 할수 있다")
+print(not 5==5) #not ture --> false
