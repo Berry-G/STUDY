4b3aca78a4238348852c6bc478a85a42bdfd3513
Young-Chae KIM <69233747+Berry-G@users.noreply.github.com>
2022-12-22 16:09:35 +0900

    2022-12-22 python study
    os
    beautifulsoup

---
+++ Machine_learning/14. 텐서플로우/딥러닝1.py
@@ -0,0 +1,15 @@
+"""
+    * AI (Artificial Intelligence) : Mimic Human Intelligence
+        - Machine Learning : Data-driven Approach
+            - 주어진 데이터나 과거 용례를 통하여 문제에 대한 해결 능력을 최대화 하는 것
+                - 현실의 자료를 반영
+            - "통계학"은 큰 역할 (샘플에서 추론)
+                - 데이터를 연구하는 학문
+                - 현실을 수학적으로 분석
+                - 규칙성을 발견, 객관적 의사결정
+            - Deep Learning : Deep Neural Networks
+                - 인공지능이라는 거대한 학문의 일부
+                - ML의 여러 기법 중 하나
+
+
+"""
